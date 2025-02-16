c48291584da2a078d1642717bf878f899aff5b22
Linus Boehm <letters@linusboehm.de>
2025-02-16 11:11:42 +0100

    Move api to hono

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,16 +1,8 @@
 import 'server-only';
 
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '@prisma/client';
-import ws from 'ws';
 import { keys } from './keys';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 
-neonConfig.webSocketConstructor = ws;
+import * as schema from './schema';
 
-const pool = new Pool({ connectionString: keys().DATABASE_URL });
-const adapter = new PrismaNeon(pool);
-
-export const database = new PrismaClient({ adapter });
-
-export * from '@prisma/client';
+export const database = drizzle({ connection: keys().DATABASE_URL, schema });
